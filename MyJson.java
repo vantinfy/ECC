15db93c559de102e5c45d4a8a26d50f4f3e62928
vantinfy <vantinfy@gmail.com>
2022-08-04 17:59:52 +0800

    MyJson类toString方法增加切片参数的处理

--- MyJson.java
+++ MyJson.java
@@ -1,4 +1,5 @@
 package ECC;
+
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
@@ -55,10 +56,36 @@ public class MyJson {
             } else if (value instanceof String) {
                 res += String.format("\"%s\":\"%s\",", key, value);
             } else if (value instanceof Double || value instanceof Float) { // 实际上小数都会被视为double
+                // 如果使用%f——String.format("%f", floatNum)——来格式化double数据会出现3.5->3.500000
+                // 3.5555555->3.555556这样的情况 因此这里使用%s
                 res += String.format("\"%s\":%s,", key, value.toString());
+            } else if (value instanceof Object[]) {
+                // 如果需要key-value的value是string，在下面第二个%s前后增加\"即可
+                res += String.format("\"s\":%s,", parseSlice((Object[]) value));
             } // value 尚不支持map set list等其它类型
         }
         return res;
+    }
+
+    // 增加对切片参数的处理
+    private String parseSlice(Object[] slice) {
+        String fin = "[";
+        // 默认format
+        String format = "%s,";
+        // string切片的format与其它的不同
+        if (slice instanceof String[]) {
+            format = "\\\"%s\\\",";
+        }
+
+        for (int i = 0; i < slice.length; i++) {
+            // %s 原因参考上面double/float的处理 int使用%s也正常故统一
+            fin += String.format(format, slice[i]);
+        }
+        fin += "]";
+        // 去掉切片中的最后一个,号
+        fin = fin.replace(",]", "]");
+
+        return fin;
     }
 
     public static void main(String[] args) {
@@ -78,9 +105,15 @@ public class MyJson {
         child2.put("language", 2);
         child2.put("type", "type");
 
+        MyJson sliceChild = new MyJson();
+        sliceChild.put("users", new String[] { "user1", "user2", "user3" });
+        sliceChild.put("ids", new Integer[] { 1, 2, 3 });
+        sliceChild.put("scores", new Double[] { 3.5555555555, 4.2, 3.3 }); // 绩点(bushi
+
         MyJson parent = new MyJson();
         parent.put("1key", child1);
         parent.put("2key", child2);
+        parent.put("slice_child", sliceChild);
         parent.put("2Key", "hello");
 
         System.out.println("toString result: " + parent.toString());
